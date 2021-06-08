0b360f1216ce542ee30fcb90121f2a21dab540a5
Matias <matiasalarcon55@hotmail.com>
2021-06-08 17:40:22 -0300

    Creacion de panel de usuarios

--- screens/UserList.js
+++ screens/UserList.js
@@ -1,14 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView, Button } from 'react-native';
 import { styles } from '../styles/generalStyles';
+import firebase from '../database/firebase';
+import { ListItem, Avatar } from 'react-native-elements';
 
-const UserList = () => {
+const UserList = (props) => {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        firebase.db.collection('users').onSnapshot((querySnapshot) => {
+            const users = [];
+            querySnapshot.docs.forEach((doc) => {
+                const { name, email, phone } = doc.data();
+                users.push({
+                    id: doc.id,
+                    name,
+                    email,
+                    phone,
+                });
+            });
+            setUsers(users);
+        });
+    }, []);
+
     return (
-        <View style={styles.container}>
-            <Text>User List</Text>
-            <StatusBar style='auto' />
-        </View>
+        <ScrollView>
+            <Button title='Create User' onPress={() => props.navigation.navigate('CreateUser')} />
+            {users.map((user) => {
+                return (
+                    <ListItem key={user.id} onPress={() => props.navigation.navigate('UserDetails', { id: user.id })}>
+                        <ListItem.Chevron />
+                        <Avatar source={{ uri: 'https://picsum.photos/200' }} rounded />
+                        <ListItem.Content>
+                            <ListItem.Title>{user.name}</ListItem.Title>
+                            <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
+                        </ListItem.Content>
+                    </ListItem>
+                );
+            })}
+        </ScrollView>
     );
 };
 
