54828610000612cbc2ee79fe6156cd64da1c9d0c
Matias <matiasalarcon55@hotmail.com>
2021-06-08 18:07:19 -0300

    Header oculto

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ const Stack = createStackNavigator();
 export default function App() {
     return (
         <NavigationContainer>
-            <Stack.Navigator>
+            <Stack.Navigator screenOptions={{ headerShown: false }}>
                 <Stack.Screen name='UserList' component={UserList} />
                 <Stack.Screen name='CreateUser' component={CreateUser} />
                 <Stack.Screen name='UserDetails' component={UserDetails} />
